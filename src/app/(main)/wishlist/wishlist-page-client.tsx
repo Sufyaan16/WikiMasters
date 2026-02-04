8eecc687cef3ab125c97a58e9e79a009142b190f
Sufyaan-16 <swen231102016@kfueit.edu.pk>
2026-02-04 21:20:53 +0500

    no-changes only commiting for consistency

--- src/app/(main)/wishlist/wishlist-page-client.tsx
+++ src/app/(main)/wishlist/wishlist-page-client.tsx
@@ -305,7 +305,7 @@ export function WishlistPageClient() {
               In Stock
             </Label>
           </div>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 ">
             <Checkbox
               id="out-of-stock"
               checked={showOutOfStock}
